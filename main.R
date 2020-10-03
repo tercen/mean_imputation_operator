24dfba9ae96bae134e3af2efdf6c9f27078efa6e
agouy <contact@agouy.com>
2020-10-03 18:06:32 +0000

    first version

--- main.R
+++ main.R
@@ -1,9 +1,16 @@
 library(tercen)
 library(dplyr)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+mat <- (ctx = tercenCtx()) %>%
+  as.matrix %>% 
+  t %>%
+  as.data.frame
+
+mat[] <- lapply(mat, function(x) ifelse(is.na(x) | is.nan(x), mean(x, na.rm = TRUE), x)) 
+colnames(mat) <- 1:ncol(mat) - 1
+
+matrix <- t(mat)
+ctx %>% select(.ci, .ri) %>% mutate(mean_imputed = as.double(matrix)) %>%
   ctx$addNamespace() %>%
   ctx$save()
+
--- workspace.R
+++ workspace.R
@@ -1,15 +1,19 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "b5eee6b1ed83d50347d04d6ba20a0f29")
+options("tercen.stepId"     = "adf28ca1-c14f-43ae-95a8-c78c2a5b1c56")
 
-getOption("tercen.workflowId")
-getOption("tercen.stepId")
+mat <- (ctx = tercenCtx()) %>%
+  as.matrix %>% 
+  t %>%
+  as.data.frame
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
-  ctx$addNamespace() %>%
-  ctx$save()
+mat[] <- lapply(mat, function(x) ifelse(is.na(x) | is.nan(x), mean(x, na.rm = TRUE), x)) 
+colnames(mat) <- 1:ncol(mat) - 1
+
+matrix <- t(mat)
+ctx %>% select(.ci, .ri) %>% mutate(mean_imputed = as.double(matrix)) %>%
+    ctx$addNamespace() %>%
+    ctx$save()
+
