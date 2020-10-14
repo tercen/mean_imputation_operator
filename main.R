0642f67aead1a128a500784544de7351d59c81b7
Alexandre Gouy <contact@agouy.com>
2020-10-14 11:01:30 +0200

    Update main.R

--- main.R
+++ main.R
@@ -2,15 +2,15 @@ library(tercen)
 library(dplyr)
 
 mat <- (ctx = tercenCtx()) %>%
-  as.matrix %>% 
+  as.matrix(fill = NA) %>% 
   t %>%
   as.data.frame
 
 mat[] <- lapply(mat, function(x) ifelse(is.na(x) | is.nan(x), mean(x, na.rm = TRUE), x)) 
 colnames(mat) <- 1:ncol(mat) - 1
 
-matrix <- t(mat)
-ctx %>% select(.ci, .ri) %>% mutate(mean_imputed = as.double(matrix)) %>%
+mat_out <- t(mat)
+ctx %>% select(.ci, .ri) %>% mutate(mean_imputed = as.double(mat_out)) %>%
   ctx$addNamespace() %>%
   ctx$save()
 
